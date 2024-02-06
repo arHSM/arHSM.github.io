e78c9c1221fa04d5da9fdca797f16590cfdf9753
한승민 <hanseungmin.ar@gmail.com>
2024-02-07 02:54:51 +0900

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
+    site: "https://arhsm.cat",
     integrations: [mdx(), sitemap(), tailwind()],
 });
